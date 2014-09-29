56d49102c3fb29140a10c655d44f3a519e814d7c
Edgardo Jimenez <edgardo.jimenez@live.com>
2014-09-28 21:49:36 -0500

    added console statements to track loading

--- www/app/app.js
+++ www/app/app.js
@@ -48,43 +48,6 @@
                 }).otherwise({ redirectTo: '/' });
         });
 
-    angular
-        .module('GSVolunteeringEvents')
-        .controller('MasterController', MasterController);
-
-    MasterController.$inject = ['$rootScope', 'statsService'];
-
-    function MasterController($rootScope, statsService) {
-        console.log('-- In MasterController');
-        var vm = this;
-
-        init();
-
-        function init() {
-            $rootScope.$on("$routeChangeStart", function(){
-                console.log('--- In MasterController  $rootScope.loading = true');
-                $rootScope.loading = true;
-            });
-
-            $rootScope.$on("$routeChangeSuccess", function(){
-                console.log('--- In MasterController  $rootScope.loading = false');
-                $rootScope.loading = false;
-            });
-
-            console.log("APP - before updateStats");
-            vm.updateStats = updateStats;
-
-            updateStats();
-            console.log("APP - before updateStats");
-        }
-
-        function updateStats() {
-            vm.stats = statsService.stats();
-        }
-
-    }
-
-
 })();
 
 
