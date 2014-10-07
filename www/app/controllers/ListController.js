5508e69970805377b72561481226e021b3b1ce24
Edgardo Jimenez <edgardo.jimenez@live.com>
2014-10-06 19:53:55 -0500

    Fixing issues with event not removing

--- www/app/controllers/ListController.js
+++ www/app/controllers/ListController.js
@@ -10,9 +10,9 @@
         .module('GSVolunteeringEvents')
         .controller('ListController', ListController);
 
-    ListController.$inject = ['$scope', 'repository', 'cordovaService', 'messageBusService'];
+    ListController.$inject = ['repository', 'cordovaService', 'messageBusService'];
 
-    function ListController($scope, repoService, cordovaService, messageBusService) {
+    function ListController(repoService, cordovaService, messageBusService) {
         /* jshint validthis: true */
         var vm = this;
 
